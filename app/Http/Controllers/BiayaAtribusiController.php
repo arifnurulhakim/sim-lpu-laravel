7cd5882126cb88b9db23a8931b3518f5a09c324b
arifnurulhakim <arifnhakim23@gmail.com>
2024-03-06 19:45:44 +0700

    refactoring

--- app/Http/Controllers/BiayaAtribusiController.php
+++ app/Http/Controllers/BiayaAtribusiController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\BiayaAtribusi;
 use App\Models\BiayaAtribusiDetail;
 use App\Models\Status;
-// use App\Models\VerifikasiBiayaAtribusi;
-// use App\Models\VerifikasiBiayaAtribusiDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -77,6 +75,20 @@ class BiayaAtribusiController extends Controller
     public function getPerTahun(Request $request)
     {
         try {
+
+            $validator = Validator::make($request->all(), [
+                'tahun' => 'nullable|numeric', // Menyatakan bahwa tahun bersifat opsional dan harus berupa angka
+                'triwulan' => 'nullable|numeric|in:1,2,3,4', // Menyatakan bahwa triwulan bersifat opsional, harus berupa angka, dan nilainya hanya boleh 1, 2, 3, atau 4
+                'status' => 'nullable|string|in:7,9', // Menyatakan bahwa status bersifat opsional, harus berupa string, dan nilainya hanya boleh "aktif" atau "nonaktif"
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $validator->errors(),
+                    'error_code' => 'INPUT_VALIDATION_ERROR',
+                ], 422);
+            }
+
             $offset = request()->get('offset', 0);
             $limit = request()->get('limit', 100);
             $search = request()->get('search', '');
@@ -84,12 +96,6 @@ class BiayaAtribusiController extends Controller
             $tahun = request()->get('tahun', '');
             $triwulan = request()->get('triwulan', '');
             $status = request()->get('status', '');
-
-            $validator = Validator::make($request->all(), [
-                'tahun' => 'nullable|numeric', // Menyatakan bahwa tahun bersifat opsional dan harus berupa angka
-                'triwulan' => 'nullable|numeric|in:1,2,3,4', // Menyatakan bahwa triwulan bersifat opsional, harus berupa angka, dan nilainya hanya boleh 1, 2, 3, atau 4
-                'status' => 'nullable|string|in:7,9', // Menyatakan bahwa status bersifat opsional, harus berupa string, dan nilainya hanya boleh "aktif" atau "nonaktif"
-            ]);
 
             $defaultOrder = $getOrder ? $getOrder : "nama ASC";
             $orderMappings = [
@@ -144,10 +150,8 @@ class BiayaAtribusiController extends Controller
             if ($triwulan !== '') {
                 $atribusiQuery->where('biaya_atribusi.triwulan', $triwulan);
             }
-
             if ($status !== '') {
-                // Anda perlu menyesuaikan kondisi WHERE ini sesuai dengan struktur tabel dan kondisi yang diinginkan.
-                // Misalnya: $atribusiQuery->where('status', $status);
+                $atribusiQuery->where('biaya_atribusi.id_status', $status);
             }
 
             $atribusi = $atribusiQuery->get();
@@ -195,6 +199,20 @@ class BiayaAtribusiController extends Controller
     public function getPerRegional(Request $request)
     {
         try {
+            $validator = Validator::make($request->all(), [
+                'nama' => 'required|string',
+                'tahun' => 'nullable|numeric',
+                'triwulan' => 'nullable|numeric|in:1,2,3,4',
+                'id_regional' => 'nullable|numeric|exists:regional,id',
+                'status' => 'nullable|string|in:7,9',
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $validator->errors(),
+                    'error_code' => 'INPUT_VALIDATION_ERROR',
+                ], 422);
+            }
             $offset = request()->get('offset', 0);
             $limit = request()->get('limit', 100);
             $search = request()->get('search', '');
@@ -203,13 +221,6 @@ class BiayaAtribusiController extends Controller
             $tahun = request()->get('tahun', '');
             $triwulan = request()->get('triwulan', '');
             $status = request()->get('status', '');
-            $validator = Validator::make($request->all(), [
-                'nama' => 'required|string',
-                'tahun' => 'nullable|numeric',
-                'triwulan' => 'nullable|numeric|in:1,2,3,4',
-                'id_regional' => 'nullable|numeric|exists:regional,id',
-                'status' => 'nullable|string|in:7,9',
-            ]);
             $defaultOrder = $getOrder ? $getOrder : "kprk.id ASC";
             $orderMappings = [
                 'namaASC' => 'kprk.nama ASC',
@@ -326,6 +337,13 @@ class BiayaAtribusiController extends Controller
                 'triwulan' => 'nullable|numeric|in:1,2,3,4',
                 'id_biaya_atribusi' => 'nullable|numeric|exists:biaya_atribusi,id',
             ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $validator->errors(),
+                    'error_code' => 'INPUT_VALIDATION_ERROR',
+                ], 422);
+            }
             $defaultOrder = $getOrder ? $getOrder : "rekening_biaya.kode_rekening ASC";
             $orderMappings = [
                 'koderekeningASC' => 'rekening_biaya.koderekening ASC',
@@ -465,6 +483,7 @@ class BiayaAtribusiController extends Controller
                 'id_biaya_atribusi' => 'required|numeric|exists:biaya_atribusi,id',
                 'id_kcu' => 'required|numeric|exists:kprk,id',
             ]);
+
             if ($validator->fails()) {
                 return response()->json([
                     'status' => 'ERROR',
@@ -518,16 +537,6 @@ class BiayaAtribusiController extends Controller
         }
     }
 
-    public function show($id)
-    {
-        try {
-            $provinsi = Provinsi::where('id', $id)->first();
-            return response()->json(['status' => 'SUCCESS', 'data' => $provinsi]);
-        } catch (\Exception $e) {
-            return response()->json(['status' => 'ERROR', 'message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    }
-
     public function verifikasi(Request $request)
     {
         try {
@@ -543,38 +552,27 @@ class BiayaAtribusiController extends Controller
 
             $id_biaya_atribusi_detail = $request->id_biaya_atribusi_detail;
             $verifikasi = $request->verifikasi;
-
-            // Menghilangkan karakter "Rp.", titik ".", koma ",", dan ",00"
             $verifikasi = str_replace(['Rp.', '.', ',00'], '', $request->verifikasi);
-
-// Mengonversi string ke integer
             $verifikasiInt = intval($verifikasi);
-
-// Output integer tanpa desimal dan angka di belakang koma
-            // dd($verifikasiInt);
-
             $catatan_pemeriksa = $request->input('catatan_pemeriksa');
             $id_validator = Auth::user()->id;
             $tanggal_verifikasi = now();
-            // Ambil data biaya atribusi detail
+
             $biaya_atribusi_detail = BiayaAtribusiDetail::find($id_biaya_atribusi_detail);
 
-            // Lakukan update
             $biaya_atribusi_detail->update([
                 'verifikasi' => $verifikasiInt,
                 'catatan_pemeriksa' => $catatan_pemeriksa,
                 'id_validator' => $id_validator,
                 'tgl_verifikasi' => $tanggal_verifikasi,
             ]);
-            // dd($biaya_atribusi_detail);
-            // Cek apakah semua verifikasi != 0.00 atau tanggal verifikasi tidak null
+
             if ($biaya_atribusi_detail) {
                 $atribusi = BiayaAtribusiDetail::where('id_biaya_atribusi', $biaya_atribusi_detail->id_biaya_atribusi)->get();
                 $countValid = $atribusi->filter(function ($detail) {
                     return $detail->verifikasi != 0.00 && $detail->tgl_verifikasi !== null;
                 })->count();
 
-                // Jika jumlah BiayaAtribusiDetail yang valid sama dengan jumlah total detail, maka lakukan pembaruan pada BiayaAtribusi
                 if ($countValid === $atribusi->count()) {
                     BiayaAtribusi::where('id', $id_biaya_atribusi)->update(['id_status' => 9]);
                 }
@@ -583,78 +581,6 @@ class BiayaAtribusiController extends Controller
 
         } catch (\Exception $e) {
             return response()->json(['status' => 'ERROR', 'message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    public function destroy($id)
-    {
-        try {
-            $provinsi = Provinsi::where('id', $id)->first();
-            $provinsi->delete();
-            return response()->json(['status' => 'SUCCESS', 'message' => 'Provinsi deleted successfully']);
-        } catch (\Exception $e) {
-            return response()->json(['status' => 'ERROR', 'message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    }
-    public function syncProvinsi()
-    {
-        try {
-            // Mendefinisikan endpoint untuk sinkronisasi provinsi
-            $endpoint = 'provinsi';
-
-            // Membuat instance dari ApiController
-            $apiController = new ApiController();
-
-            // Membuat instance dari Request dan mengisi access token jika diperlukan
-            $request = new Request();
-            $request->merge(['end_point' => $endpoint]);
-
-            // Memanggil makeRequest dari ApiController untuk sinkronisasi dengan endpoint provinsi
-            $response = $apiController->makeRequest($request);
-
-            // Mengambil data provinsi dari respons
-            $dataProvinsi = $response['data'];
-            if (!$dataProvinsi) {
-                return response()->json(['message' => 'Terjadi kesalahan: sync error'], 500);
-            }
-
-            // Memulai transaksi database untuk meningkatkan kinerja
-            DB::beginTransaction();
-
-            // Memproses setiap data provinsi dari respons
-            foreach ($dataProvinsi as $data) {
-                // Mencari provinsi berdasarkan ID
-                $provinsi = Provinsi::find($data['kode_provinsi']);
-
-                // Jika provinsi ditemukan, perbarui data
-                if ($provinsi) {
-                    $provinsi->update([
-                        'nama' => $data['nama_provinsi'],
-                        // Perbarui atribut lain yang diperlukan
-                    ]);
-                } else {
-                    // Jika provinsi tidak ditemukan, tambahkan data baru
-                    Provinsi::create([
-                        'id' => $data['kode_provinsi'],
-                        'nama' => $data['nama_provinsi'],
-                        // Tambahkan atribut lain yang diperlukan
-                    ]);
-                }
-            }
-
-            // Commit transaksi setelah selesai
-            DB::commit();
-
-            // Setelah sinkronisasi selesai, kembalikan respons JSON sukses
-            return response()->json([
-                'status' => 'SUCCESS',
-                'message' => 'Sinkronisasi provinsi berhasil'], 200);
-        } catch (\Exception $e) {
-            // Rollback transaksi jika terjadi kesalahan
-            DB::rollBack();
-
-            // Tangani kesalahan yang terjadi selama sinkronisasi
-            return response()->json(['message' => 'Terjadi kesalahan: ' . $e->getMessage()], 500);
         }
     }
 }
